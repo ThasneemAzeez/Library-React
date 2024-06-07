b18664d87540cd9c156cdc1bf93ecb6547a8b64b
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-07 12:57:07 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
 import SearchBook from './components/SearchBook';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div >
       <AddBook/>
       <SearchBook/>
+      <Delete/>
       
     </div>
   );
